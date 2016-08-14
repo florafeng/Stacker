08667f83b2cb8f5d81ac4cb92fe7c2f8333e6711
Paul de Bruyn <hpdb718265@gmail.com>
2016-08-14 19:19:06 +0000

    Merging Changes up to this point

--- app/actions.rb
+++ app/actions.rb
@@ -8,11 +8,6 @@ end
 # Login validation.  This action doesn't render anything - just redirects to
 # two other actions depending on a successful login.
 
-get '/test' do
-  @test_variable = "I'M ACTUALLY HERE"
-  erb :test
-end
-
 post '/validate' do
   email = params[:email]
   password = params[:password]
@@ -24,7 +19,7 @@ post '/validate' do
     session.delete(:login_error)
     session[:user_id] = user.id
     session[:current_user] = user.id
-    redirect '/user_query'
+    redirect '/'
   else
     user = User.find_by(email: email)
     if user
@@ -43,7 +38,7 @@ post '/validate' do
       if user.save
         session.delete(:login_error)
         session[:user_id] = user.id
-        redirect "/user_query"
+        redirect "/"
       else
 
 # This should never happen
@@ -66,62 +61,69 @@ get '/' do
   erb :index
 end
 
-get '/user_query' do
-  erb :user_query
-end
-
-#post '/' do
-#    @query = params[:search]
-    # @response_hash = question_title_by_votes
-    # @q_title = get_q_title
-#end
-
-
+# Pull out all content from stackoverflow
 post '/' do
-    @query = params[:search]
-    response_hash
-    get_data
-    erb :search_result
-
-    # redirect '/search_result'
+  @query = params[:search]
+  @response_array = get_data(response_hash(@query))
+  SavedPost.delete_all
+  erb :search_result
 end
 
-# get "/search_result" do
-#   # binding.pry
-#   erb :search_result
+post '/saved_post' do
+  # binding.pry
+  saved_post
+  if session[:current_user].nil?
+  else
+  @saved_post = SavedPost.where(user_id: session[:current_user])
+  end
+
+  erb :saved_post
+end
+
+# get '/saved_post' do
+#   @saved_post = SavedPost.where(user_id: session[:current_user])
+#   erb :saved_post
 # end
 
-# get '/ans' do
-# 	@response_hash = question_title_by_votes
-#     @q_title = get_q_title
-# 	erb :ans 
-# end
 
-def response_hash
+def response_hash(q)
   #/2.2/search/advanced?order=desc&sort=votes&title=selknvslk&site=stackoverflow
   url_p1 = 'http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=votes&'
   url_p3 = '&site=stackoverflow'
-  url = url_p1 + @query + url_p3
-  @response_hash = HTTParty.get(url)
+  url = url_p1 + q + url_p3
+  HTTParty.get(url)
 end
 
-def get_data
-  @response_array = []
-  @response_hash["items"].each do |item|
+def get_data(r_hash)
+  r_array = []
+  r_hash["items"].each do |item|
     q_title = item["title"]
     q_link = item["link"]
-    @nokogiri_object = Nokogiri::HTML(open(q_link))
-    q_body = @nokogiri_object.xpath("//div[@class='question']//div[@class='post-text']").to_html
-    # binding.pry
-    a_body = @nokogiri_object.xpath("//div[@class='answer accepted-answer']//div[@class='post-text']").to_html
+    nokogiri_object = Nokogiri::HTML(open(q_link))
+    q_body = nokogiri_object.xpath("//div[@class='question']//div[@class='post-text']").to_html
+    a_body = nokogiri_object.xpath("//div[@class='answer accepted-answer']//div[@class='post-text']").to_html
     so_response = Hash.new
     so_response = {
       so_question_title: q_title,
       so_question_body: q_body,
-      so_answer_body: a_body
+      so_answer_body: a_body,
+      so_link: q_link,
+      so_query: @query
       }
-    @response_array << so_response
+      # binding.pry
+    r_array << so_response
   end
-  @response_array
+  r_array
 end
 
+
+def saved_post
+  # binding.pry
+  if session[:current_user].nil?
+  else
+    saved_post = SavedPost.find_by(user_id: session[:current_user], q_link: params[:q_link])
+    if saved_post.nil?
+      SavedPost.create(user_id: session[:current_user], q_link: params[:q_link], query_text: params[:query_text], q_title: params[:q_title])
+    end
+  end
+end
